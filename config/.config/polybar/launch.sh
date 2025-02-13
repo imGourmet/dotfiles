fe59df70f9f9bda3e93cb96aa46e8ad3ac482d10
Andrés Guilluy <a_guilluy@me.com>
2025-02-13 17:22:51 +0100

    more cleaning

--- config/.config/polybar/launch.sh
+++ config/.config/polybar/launch.sh
@@ -7,12 +7,6 @@
 #                                                       /_/
 #
 
-#Matar cualquier instancia de Redshift y esperar a que termine
-#pkill redshift-gtk && sleep 1
-#while pgrep -u $UID -x redshift-gtk >/dev/null; do sleep 1; done
-# Lanzar Redshift
-#redshift-gtk &
-#echo "Redshift ha comenzado..."
 
 # Matar cualquier instancia de Polybar y esperar a que termine
 pkill polybar && sleep 1
@@ -22,8 +16,4 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 polybar --config=~/.config/polybar/config.ini
 echo "Polybar ha comenzado..."
 
-# Ejecutar plank.sh desde launch.sh
-~/.config/bspwm/scripts/plank.sh &
 
-# Descomentar la siguiente línea si usas Plank y quieres que siempre esté al frente
-xdotool search --name Plank windowraise &
